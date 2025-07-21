0d1b0990c06d3f39befe470e0b8c28f17b3e06b0
Laura V. Morales <lvmorales@baufest.com>
2025-07-21 17:12:58 -0300

    Update Scripts

--- bash/01.Create_Action_Group_and_Budget.sh
+++ bash/01.Create_Action_Group_and_Budget.sh
@@ -21,9 +21,9 @@ ActionGroupId=$(az monitor action-group create \
 echo "Action Group created with ID: $ActionGroupId"
 
 # Add email receiver to the action group
-az monitor action-group email add \
+az monitor action-group enable-receiver add \
             --resource-group $AZURE_ACTION_GROUP_RG \
-            --action-group $AZURE_ACTION_TAG_NAME \
+            --name $AZURE_ACTION_TAG_NAME \
             --action-name $AZURE_ACTION_GROUP_EMAIL_RECIEVER_NAME \
             --email-address $AZURE_ACTION_GROUP_EMAIL
 echo "Email receiver added to action group"
@@ -34,14 +34,16 @@ echo "Email receiver added to action group"
 az consumption budget create \
     --amount $AZURE_BUDGET_AMOUNT \
     --budget-name $AZURE_BUDGET_NAME \
-    --resource-group $AZURE_ACTION_GROUP_RG \
-    --category $AZURE_BUDGET_CATEGORY \
+    --start-date $AZURE_BUDGET_START_DATE \
+    --end-date $AZURE_BUDGET_END_DATE \
     --time-grain $AZURE_BUDGET_TIME_GRAIN \
+    --category $AZURE_BUDGET_CATEGORY \
+    --resource-group $AZURE_ACTION_GROUP_RG \
     --time-period "{\"startDate\":\"$AZURE_BUDGET_START_DATE\",\"endDate\":\"$AZURE_BUDGET_END_DATE\"}" \
     --notifications "{\"$AZURE_BUDGET_NOTIFICATION_KEY\":{\"enabled\":$AZURE_BUDGET_NOTIFICATION_ENABLED, \"operator\":\"$AZURE_BUDGET_NOTIFICATION_OPERATOR\", \"contactEmails\":[\"$AZURE_ACTION_GROUP_EMAIL\"], \"threshold\":$AZURE_BUDGET_NOTIFICATION_THRESHOLD, \"contactGroups\":[\"$ActionGroupId\"]}}"
 echo "Budget created with ID: $AZURE_BUDGET_NAME"
 echo "Budget creation completed with Action Group ID: $ActionGroupId"
 # Display the created budget details
-az consumption budget show --name "$AZURE_BUDGET_NAME" --resource-group "$AZURE_ACTION_GROUP_RG" --output table
+az consumption budget show --budget-name "$AZURE_BUDGET_NAME" --resource-group "$AZURE_ACTION_GROUP_RG" --output table
 
 
