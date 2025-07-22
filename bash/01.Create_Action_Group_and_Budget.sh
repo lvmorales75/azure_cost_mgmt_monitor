65b10705edab303d34f4ef43155c402ce11e2039
Laura V. Morales <lvmorales@baufest.com>
2025-07-22 11:18:34 -0300

    Update Script with Budget Creation

--- bash/01.Create_Action_Group_and_Budget.sh
+++ bash/01.Create_Action_Group_and_Budget.sh
@@ -28,35 +28,46 @@ ActionGroupId=$(az monitor action-group create \
             --output tsv)
 echo "Action Group created with ID: $ActionGroupId"
 
-# Add email receiver to the action group
-echo "command: az monitor action-group enable-receiver add \
-            --name $AZURE_ACTION_GROUP_EMAIL_RECIEVER_NAME \
+# Add receiver to the action group
+echo "command: az monitor action-group enable-receiver \
+            --receiver-name $AZURE_ACTION_GROUP_EMAIL_RECEIVER_NAME \
             --resource-group $AZURE_RESOURCE_GROUP \
             --action-group-name $AZURE_ACTION_GROUP_NAME"
 
 az monitor action-group enable-receiver \
-            --name $AZURE_ACTION_GROUP_EMAIL_RECIEVER_NAME \
+            --receiver-name $AZURE_ACTION_GROUP_EMAIL_RECEIVER_NAME \
             --resource-group $AZURE_RESOURCE_GROUP \
             --action-group-name $AZURE_ACTION_GROUP_NAME
-echo "Email receiver added to action group"
+echo "Receiver added to action group"
 
- 
+
 # Create a monthly budget that sends an email and triggers an Action Group to send a second email.
 # Make sure the StartDate for your monthly budget is set to the first day of the current month.
 # Note that Action Groups can also be used to trigger automation such as Azure Functions or Webhooks.
-#az consumption budget create \
-#    --amount $AZURE_BUDGET_AMOUNT \
-#    --budget-name $AZURE_BUDGET_NAME \
-#    --start-date $AZURE_BUDGET_START_DATE \
-#    --end-date $AZURE_BUDGET_END_DATE \
-#    --time-grain $AZURE_BUDGET_TIME_GRAIN \
-#    --category $AZURE_BUDGET_CATEGORY \
-#    --resource-group $AZURE_ACTION_GROUP_RG \
-#    --time-period "{\"startDate\":\"$AZURE_BUDGET_START_DATE\",\"endDate\":\"$AZURE_BUDGET_END_DATE\"}" \
-#    --notifications "{\"$AZURE_BUDGET_NOTIFICATION_KEY\":{\"enabled\":$AZURE_BUDGET_NOTIFICATION_ENABLED, \"operator\":\"$AZURE_BUDGET_NOTIFICATION_OPERATOR\", \"contactEmails\":[\"$AZURE_ACTION_GROUP_EMAIL\"], \"threshold\":$AZURE_BUDGET_NOTIFICATION_THRESHOLD, \"contactGroups\":[\"$ActionGroupId\"]}}"
-#echo "Budget created with ID: $AZURE_BUDGET_NAME"
-#echo "Budget creation completed with Action Group ID: $ActionGroupId"
+echo "command: az consumption budget create \
+            --amount $AZURE_BUDGET_AMOUNT \
+            --budget-name $AZURE_BUDGET_NAME \
+            --category $AZURE_BUDGET_CATEGORY \            
+            --start-date $AZURE_BUDGET_START_DATE \
+            --end-date $AZURE_BUDGET_END_DATE \
+            --time-grain $AZURE_BUDGET_TIME_GRAIN \
+            --resource-group $AZURE_ACTION_GROUP_RG \
+
+az consumption budget create \
+    --amount $AZURE_BUDGET_AMOUNT \
+    --budget-name $AZURE_BUDGET_NAME \
+    --category $AZURE_BUDGET_CATEGORY \
+    --start-date $AZURE_BUDGET_START_DATE \
+    --end-date $AZURE_BUDGET_END_DATE \
+    --time-grain $AZURE_BUDGET_TIME_GRAIN \
+    --resource-group $AZURE_ACTION_GROUP_RG \
+echo "Budget created with ID: $AZURE_BUDGET_NAME"
+echo "Budget creation completed with Action Group ID: $ActionGroupId"
+
 # Display the created budget details
-#az consumption budget show --budget-name "$AZURE_BUDGET_NAME" --resource-group "$AZURE_ACTION_GROUP_RG" --output table
+az consumption budget show 
+    --budget-name "$AZURE_BUDGET_NAME" 
+    --resource-group "$AZURE_ACTION_GROUP_RG" 
+    --output table
 
 
